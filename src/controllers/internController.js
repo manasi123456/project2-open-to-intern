cf108fae97d2843f4e9b9542bf8ec3aab9ba33e5
akshintalaabhijith <akshintala.abhijith@gmail.com>
2022-05-06 15:08:20 +0530

    project2 done

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -34,7 +34,7 @@ const createIntern = async function (req, res) {
 
     // Destructuring body
 
-    const { name, email, mobile, collegeId } = requestBody;
+    const { name, email, mobile, collegeName } = requestBody;
 
     // Validation Starts
 
@@ -49,8 +49,8 @@ const createIntern = async function (req, res) {
         .status(400)
         .send({ status: false, message: "Email is required" });
     }
-    let checkEmail = await internModel.findOne({email: requestBody.email })
-            if (checkEmail) return res.status(400).send({ msg: "Email already exist" })
+    let checkEmail = await internModel.findOne({ email: requestBody.email });
+    if (checkEmail) return res.status(400).send({ status: false, message: "Email already exist" });
 
     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
       return res
@@ -63,42 +63,48 @@ const createIntern = async function (req, res) {
         .status(400)
         .send({ Status: false, message: "Mobile Number is required" });
     }
-    let checkMobile = await internModel.findOne({mobile: requestBody.mobile })
-            if (checkMobile) return res.status(400).send({ msg: "Mobile Number already exist" })
-    
+    let checkMobile = await internModel.findOne({ mobile: requestBody.mobile });
+    if (checkMobile)
+      return res.status(400).send({status: false, message: "Mobile Number already exist" });
 
     if (
-      !/^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(mobile)
+      !/^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(
+        mobile
+      )
     ) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: "Mobile Number is not in correct format",
-        });
+      return res.status(400).send({
+        status: false,
+        message: "Mobile Number is not in correct format",
+      });
     }
 
-    if (!isValidObjectId(collegeId)) {
+    if (!isValid(collegeName)) {
       return res
         .status(400)
-        .send({ status: false, message: "Enter a valid ObjectId" });
-        }
+        .send({ status: false, message: "College Name is required" });
+    }
 
-        let checkcollegeId = await collegeModel.findOne({_id: requestBody.collegeId })
-        console.log(checkcollegeId)
-            if (checkcollegeId==null) return res.status(400).send({ msg: "College doesn't exist" })
+    let collegeId = await collegeModel.findOne({ name: collegeName }).select({_id : 1})
+    // console.log(collegeId)
+    if (!collegeId)
+      return res.status(400).send({ status: false, message: "College doesn't exist" });
+
+    let obj = {
+      name: name,
+      email: email,
+      mobile: mobile,
+      collegeId: collegeId._id,
+    };
 
     //Creating Intern
 
-    const intern = await internModel.create(requestBody);
+    const intern = await internModel.create(obj);
 
-    res
-      .status(201)
-      .send({
-        status: true,
-        message: "Intern created successfully",
-        data: intern,
-      });
+    res.status(201).send({
+      status: true,
+      message: "Intern created successfully",
+      data: intern,
+    });
   } catch (error) {
     res.status(500).send({ status: false, messsage: error.message });
   }
